c1d8d531c18b135e7a9ddf33ff0f760124ce71f6
alicantarim <alicantarim@hotmail.com>
2024-10-25 00:40:05 +0300

    worked with button and text components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Button from "./components/Button";
+import Text from "./components/Text";
+
 function App() {
-  return <div>React Eğitimi</div>;
+  //props ... Veri aktarma.
+  return (
+    <>
+      <Text number="1" name={"React"} />
+      <Text number="2" name={"Reactt"} />
+      <Text number="3" name={"Reacttt"} />
+      <Button name={"Artır"} />
+      <Button name={"Azalt"} />
+    </>
+  );
 }
 
 export default App;
